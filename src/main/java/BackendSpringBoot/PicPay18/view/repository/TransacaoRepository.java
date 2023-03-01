41af98cc304b59fba316a77f6280d9da92a76fdd
emersonamorim-dev <emerson_tecno@hotmail.com>
2023-03-01 07:40:57 -0300

    Update TransacaoRepository.java

--- src/main/java/BackendSpringBoot/PicPay18/view/repository/TransacaoRepository.java
+++ src/main/java/BackendSpringBoot/PicPay18/view/repository/TransacaoRepository.java
@@ -1,11 +1,11 @@
 package BackendSpringBoot.PicPay18.view.repository;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import BackendSpringBoot.PicPay18.model.Transacao;
 
 @Repository
-public interface TransacaoRepository extends MongoRepository<Transacao, String> {
+public interface TransacaoRepository extends JpaRepository<Transacao, String> {
 
 }
