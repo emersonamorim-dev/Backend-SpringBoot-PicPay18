f692004f1e752be85d1ea6df974a890602bb6f36
emersonamorim-dev <emerson_tecno@hotmail.com>
2023-03-01 07:40:40 -0300

    Update UsuarioRepository.java

--- src/main/java/BackendSpringBoot/PicPay18/view/repository/UsuarioRepository.java
+++ src/main/java/BackendSpringBoot/PicPay18/view/repository/UsuarioRepository.java
@@ -2,18 +2,17 @@ package BackendSpringBoot.PicPay18.view.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import BackendSpringBoot.PicPay18.model.Usuario;
 
 @Repository
-public interface UsuarioRepository extends MongoRepository<Usuario, String> {
-Usuario findByEmail(String email);
+public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+    Usuario findByEmail(String email);
 
-void deleteById(Long id);
+    void deleteById(Long id);
 
-Optional<Usuario> findById(Long id);
-
+    Optional<Usuario> findById(Long id);
 
 }
