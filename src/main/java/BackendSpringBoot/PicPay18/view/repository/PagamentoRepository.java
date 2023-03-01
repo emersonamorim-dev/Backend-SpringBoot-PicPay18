8d4b3e918feaeaf15cdbd8ccea304783bf258a5a
emersonamorim-dev <emerson_tecno@hotmail.com>
2023-03-01 07:41:15 -0300

    Update PagamentoRepository.java

--- src/main/java/BackendSpringBoot/PicPay18/view/repository/PagamentoRepository.java
+++ src/main/java/BackendSpringBoot/PicPay18/view/repository/PagamentoRepository.java
@@ -1,14 +1,13 @@
 package BackendSpringBoot.PicPay18.view.repository;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import BackendSpringBoot.PicPay18.model.Pagamento;
 import BackendSpringBoot.PicPay18.model.Transacao;
 
 @Repository
-public interface PagamentoRepository extends MongoRepository<Transacao, String> {
+public interface PagamentoRepository extends JpaRepository<Transacao, String> {
 
     Pagamento save(Pagamento pagamento);
-
 }
